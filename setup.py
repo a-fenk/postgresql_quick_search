89e8cf326ae08e59a19630d35cb497761f549376
Artem Antonov <54385446+a-fenk@users.noreply.github.com>
2022-06-09 23:00:18 +0300

    fix_setup

--- setup.py
+++ setup.py
@@ -4,20 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="jwt-service",
+    name="postgresql-quick-search",
     version="0.0.1",
     author="Artem Antonov",
     author_email="artyom.antnv@gmail.com",
     description="...",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitlab.com/skipp1/S954_Skipp3_0/jwt",
+    url="https://gitlab.com/skipp1/S954_Skipp3_0/postgresql_quick_search",
     packages=setuptools.find_packages(),
-    install_requires=[
-        'pydantic==1.9.0',
-        'PyJWT==2.4.0',
-        'typing_extensions==4.0.1',
-    ],
+    install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
